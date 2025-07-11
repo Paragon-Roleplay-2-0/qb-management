be58b12d6b153b7b733f2ca64664fce90534b1b7
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2025-07-10 18:08:35 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 author 'Kakarot'
 description 'Employee management system allowing players to hire/fire other players'
-version '2.1.3'
+version '2.1.4'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
